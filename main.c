058343f1975fdd7dc0911dd50febff68a1c301a0
Felipe Marques <feelipemarquees@gmail.com>
2026-01-03 10:55:37 -0300

    Added help command to shell

--- main.c
+++ main.c
@@ -10,23 +10,42 @@ int cmd_exit(char *input){
     return 0;
 }
 
+int cmd_help(char *input){
+    printf("Command list:\n");
+    printf("HELP    shows the command list\n");
+    printf("EXIT    closes the shell\n");
+
+    return 2;
+}
+
 int main(){
     
     Comandos comandos[] = {
         {"exit", cmd_exit},
+        {"help", cmd_help}
     };
 
-    while(1){
+    int found = 0;
+    int nextLine = 1;
+
+    while(nextLine){
         char input[1024];
         printf("myshell> ");
         fgets(input, sizeof(input), stdin);
         
         input[strcspn(input, "\n")] = '\0';
 
-        for(int i = 0; i <= 10; i++){
+        for(int i = 0; i < sizeof(comandos)/sizeof(comandos[0]); i++){
             if(strcmp(input, comandos[i].nome) == 0){
-                
+                found++;
+                nextLine = comandos[i].acao(input);
             }
         }
+
+        if(found == 0){
+            printf("Command not found! Try 'help'!\n\n");
+        }
+
+        found = 0;
     }
 }
