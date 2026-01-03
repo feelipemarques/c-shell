517022a119d753782da1e54b6b7a2cf86bf6f852
Felipe Marques <feelipemarquees@gmail.com>
2026-01-03 18:10:45 -0300

    Added version command

---
+++ src/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct command{
+    char name[16];
+    int (*action)(char *);
+} Commands;
+
+int cmd_exit(char *input){
+    return 0;
+}
+
+int cmd_help(char *input){
+    printf("Command list:\n");
+    printf("HELP        shows the command list\n");
+    printf("EXIT        closes the shell\n");
+    printf("VERSION     shows the C-SHELL's current version\n");
+    return 1;
+}
+
+int cmd_version(char *input){
+    printf("C-Shell v0.1.0\n\n");
+    return 1;
+}
+
+
+int main(){
+    
+    Commands commands[] = {
+        {"exit", cmd_exit},
+        {"help", cmd_help},
+        {"version", cmd_version}
+    };
+
+    int found = 0;
+    int running = 1;
+
+    while(running){
+        char input[1024];
+        printf("myshell> ");
+        fgets(input, sizeof(input), stdin);
+        
+        input[strcspn(input, "\n")] = '\0';
+
+        for(int i = 0; i < sizeof(commands)/sizeof(commands[0]); i++){
+            if(strcmp(input, commands[i].name) == 0){
+                found++;
+                running = commands[i].action(input);
+            }
+        }
+
+        if(found == 0){
+            printf("Command not found! Try 'help'!\n\n");
+        }
+
+        found = 0;
+    }
+}
