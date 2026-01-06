caf1b27e0c93e5b5db38a927abd050eee913d86a
Felipe Marques <feelipemarquees@gmail.com>
2026-01-06 13:02:13 -0300

    Fix memory garbage error in cp command

--- src/commands/cmd_cp.c
+++ src/commands/cmd_cp.c
@@ -4,13 +4,25 @@
 
 int cmd_cp(int argc, char **argv){
     int src = open(argv[1], 0);
-
     int dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 
-    char buffer[4096];
+    if(src < 0 || dest < 0){
+        perror("cp: error opening file");
+        return -1;
+    }
 
-    while(read(src, buffer, 4096) != 0){
-        write(dest, buffer, 4096);
+    char buffer[4096];
+    ssize_t bytesRead;
+
+    while((bytesRead = read(src, buffer, sizeof(buffer))) > 0){
+       write(dest, buffer, bytesRead);
+    }
+
+    if(bytesRead < 0){
+        perror("cp: error reading source file");
+        close(src);
+        close(dest);
+        return -1;
     }
 
     close(src);
