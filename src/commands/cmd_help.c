29ab2393589cbd3a7ce4fe432d60ed49e0fa0f96
Felipe Marques Sanches <feelipemarquees@gmail.com>
2026-01-08 12:41:52 -0300

    Update help command

--- src/commands/cmd_help.c
+++ src/commands/cmd_help.c
@@ -3,13 +3,19 @@
 
 int cmd_help(int argc, char **argv){
     printf("Command list:\n");
-    printf("HELP        shows the command list\n");
-    printf("EXIT        closes the shell\n");
-    printf("VERSION     shows the C-SHELL's current version\n");
-    printf("CLEAR       clears the terminal\n");
-    printf("CD          changes the directory\n");
-    printf("PWD         shows current directory\n");
-    printf("LS          lists the current directory's content\n");
-    printf("MKDIR       creates a new directory\n");
+    printf("HELP        show the command list\n");
+    printf("EXIT        close the shell\n");
+    printf("VERSION     show the C-SHELL's current version\n");
+    printf("CLEAR       clear the terminal\n");
+    printf("CD          change the directory\n");
+    printf("PWD         show current directory\n");
+    printf("LS          list the current directory's content\n");
+    printf("MKDIR       create a new directory\n");
+    printf("RMDIR       remove the specified directory\n");
+    printf("RM          remove the specified file\n");
+    printf("CAT         read the specified file\n");
+    printf("CP          create a copy from the specified file\n");
+    printf("ECHO        write arguments to the standard output\n");
+    printf("TOUCH       create a new file\n");
     return 1;
 }
